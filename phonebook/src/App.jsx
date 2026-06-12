924fdbb6d6766c8169b34d75b9673404e96aba08
itsjustyuji <vedantsharmavs2002@gmail.com>
2026-06-13 02:55:05 +0530

    Added search field that shows filtered results acc to name. Ex 2.9

--- phonebook/src/App.jsx
+++ phonebook/src/App.jsx
@@ -10,13 +10,14 @@ const Show=({persons})=>{
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { 
-      name: 'Arto Hellas',
-      number: '040-1234567'
-    }
-  ]) 
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
   const [newName, setNewName] = useState('')
   const [newNum, setNewNum] = useState('')
+  const [filter, setFilter]=useState('')
 
   const clickSubmit=(e)=>{
     e.preventDefault()
@@ -44,13 +45,24 @@ const App = () => {
   }
 
   const handleNumChange=(e)=>{
-    console.log(e.target.value)
     setNewNum(e.target.value)
   }
+
+  const handleFilterChange=(e)=>{
+    setFilter(e.target.value)
+  }
+
+  const ToShow = persons.filter(person => 
+    person.name.toLowerCase().includes(filter.toLowerCase())
+  )
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with <input value={filter} onChange={handleFilterChange}></input>
+      </div>
+      <h2>add a new</h2>
       <form onSubmit={clickSubmit}> 
         <div>
           name: <input value={newName} onChange={handleChange} />
@@ -63,7 +75,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      <div><Show persons={persons} /></div>
+      <div><Show persons={ToShow} /></div>
     </div>
   )
 }
